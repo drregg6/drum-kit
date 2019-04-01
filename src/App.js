51f8ee361d9e73f708cd9005b28297f81b32ee88
Dave <drregg6@gmail.com>
2019-04-01 08:23:05 -0400

    DRUM KIT

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react';
+import Sound from 'react-sound';
 import logo from './logo.svg';
 import './App.css';
 
+import boom from './audio/boom.wav';
+import clap from './audio/clap.wav';
+import hihat from './audio/hihat.wav';
+import kick from './audio/kick.wav';
+import openhat from './audio/openhat.wav';
+import ride from './audio/ride.wav';
+import snare from './audio/snare.wav';
+import tink from './audio/tink.wav';
+import tom from './audio/tom.wav';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playStatus: Sound.status.STOPPED,
+      url: null
+    }
+  }
+
+  handleKeyDown = (ev) => {
+    switch (ev.key) {
+      case 'a':
+        console.log('This is the letter a');
+        this.setState({
+          url: hihat,
+          playStatus: Sound.status.PLAYING
+        });
+        break;
+      case 'k':
+      console.log('This is the letter k');
+      this.setState({
+        url: ride,
+        playStatus: Sound.status.PLAYING
+      });
+      break;
+      default:
+        console.log(ev.key);
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,6 +65,10 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Sound
+          url={this.state.url}
+          playStatus={this.state.playStatus}
+        />
       </div>
     );
   }
