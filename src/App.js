10a8ccb1fcc82a9ce88be3f9bc5966c19bf0aecb
Dave <drregg6@gmail.com>
2019-04-01 08:28:54 -0400

    audio files play on keypress

--- src/App.js
+++ src/App.js
@@ -25,20 +25,69 @@ class App extends Component {
 
   handleKeyDown = (ev) => {
     switch (ev.key) {
-      case 'a':
-        console.log('This is the letter a');
+      case 'q':
+        console.log('This is the letter q');
+        this.setState({
+          url: boom,
+          playStatus: Sound.status.PLAYING
+        });
+        break;
+      case 'w':
+        console.log('This is the letter w');
+        this.setState({
+          url: clap,
+          playStatus: Sound.status.PLAYING
+        });
+        break;
+      case 'e':
+        console.log('This is the letter e');
         this.setState({
           url: hihat,
           playStatus: Sound.status.PLAYING
         });
         break;
-      case 'k':
-      console.log('This is the letter k');
-      this.setState({
-        url: ride,
-        playStatus: Sound.status.PLAYING
-      });
-      break;
+      case 'a':
+        console.log('This is the letter a');
+        this.setState({
+          url: kick,
+          playStatus: Sound.status.PLAYING
+        });
+        break;
+      case 's':
+        console.log('This is the letter s');
+        this.setState({
+          url: openhat,
+          playStatus: Sound.status.PLAYING
+        });
+        break;
+      case 'd':
+        console.log('This is the letter d');
+        this.setState({
+          url: ride,
+          playStatus: Sound.status.PLAYING
+        });
+        break;
+      case 'z':
+        console.log('This is the letter z');
+        this.setState({
+          url: snare,
+          playStatus: Sound.status.PLAYING
+        });
+        break;
+      case 'x':
+        console.log('This is the letter x');
+        this.setState({
+          url: tink,
+          playStatus: Sound.status.PLAYING
+        });
+        break;
+      case 'c':
+        console.log('This is the letter c');
+        this.setState({
+          url: tom,
+          playStatus: Sound.status.PLAYING
+        });
+        break;
       default:
         console.log(ev.key);
     }
